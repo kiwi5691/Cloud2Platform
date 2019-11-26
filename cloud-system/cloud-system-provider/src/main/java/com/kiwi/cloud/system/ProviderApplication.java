d19beea036d44a43314279af061ec9765f5dfe39
kiwi5691 <805344479@qq.com>
2019-11-26 08:48:08 +0800

    test nacos center

--- cloud-system/cloud-system-provider/src/main/java/com/kiwi/cloud/system/ProviderApplication.java
+++ cloud-system/cloud-system-provider/src/main/java/com/kiwi/cloud/system/ProviderApplication.java
@@ -3,6 +3,7 @@ package com.kiwi.cloud.system;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -21,7 +22,22 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 @EnableDiscoveryClient
 public class ProviderApplication {
+
+
+    @Value("${spring.cloud.nacos.discovery.server-addr}")
+    public static String aaa;
+    @Value("${server.port}")
+    public static String tt;
+
+    static {
+        System.out.println(tt);
+        System.out.println(aaa);
+    }
+
+
     public static void main(String[] args) {
+
+
         log.info("init center");
         SpringApplication.run(ProviderApplication.class,args);
     }
